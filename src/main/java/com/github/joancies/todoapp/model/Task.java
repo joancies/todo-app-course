5383436432d557d0342d924985eed8912aba6575
Joanna Ciesielska <ciesielskajw@gamail.com>
2023-04-26 15:18:34 +0200

    Initial commit

---
+++ src/main/java/com/github/joancies/todoapp/model/Task.java
@@ -0,0 +1,44 @@
+package com.github.joancies.todoapp.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.*;
+import jakarta.validation.constraints.NotBlank;
+
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message="Task's description must be not null or empty")
+    private String description;
+    private boolean done;
+
+    public Task() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
